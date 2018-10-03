b1a4cda3581f8495e365d21d0c614e1409444a51
Sourav Adhikari <sa00579712@techmahindra.com>
2018-10-03 16:06:27 +0530

    2 Projects added

---
+++ MessageReceiverWebJobs/MessageReceiverWebJobs/Functions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.WebJobs;
+
+namespace MessageReceiverWebJobs
+{
+    public class Functions
+    {
+        public static void ProcessQueueMessage([ServiceBusTrigger("myqueue", Connection = "AzureWebJobsServiceBus")]string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ MessageReceiverWebJobs/MessageReceiverWebJobs/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace MessageReceiverWebJobs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new HostBuilder().UseEnvironment("development")
+             .ConfigureWebJobs(b =>
+             {
+
+                 b.AddAzureStorageCoreServices()
+                 .AddAzureStorage()
+                 .AddServiceBus();
+
+             })
+
+             .ConfigureLogging((context, b) =>
+             {
+                 b.SetMinimumLevel(LogLevel.Debug);
+                 b.AddConsole();
+
+             });
+
+            var host = builder.Build();
+            using (host)
+            {
+                host.Run();
+            }
+        }
+    }
+}
