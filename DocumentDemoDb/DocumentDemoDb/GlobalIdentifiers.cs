07763ff79327c064924352b06d187a41662cca27
Dileep Dayanand Pai <DP00557033@techmahindra.com>
2018-10-05 12:21:47 +0530

    Entered Document Db with Some changes

--- DocumentDemoDb/DocumentDemoDb/GlobalIdentifiers.cs
+++ DocumentDemoDb/DocumentDemoDb/GlobalIdentifiers.cs
@@ -8,13 +8,10 @@ namespace DocumentDemoDb
     public static class GlobalIdentifiers
     {
 
-
-        public static string EndPoint = "<Document Db End Point obtained from Portal>";
-        //Environment.GetEnvironmentVariable("EndPointOfDb");
-        public static string AuthKey = "<Document Db connection String from Portal>";
-            //Environment.GetEnvironmentVariable("AuthKey");
-        public static string MyLinkCollection = "dbs/your Db name/colls/your coloumn name/";
-            //Environment.GetEnvironmentVariable("MyLinkCollection");
+       
+        public static string EndPoint = Environment.GetEnvironmentVariable("EndPointOfDb");
+        public static string AuthKey = Environment.GetEnvironmentVariable("AuthKey");
+        public static string MyLinkCollection = Environment.GetEnvironmentVariable("MyLinkCollection");
 
     }
 }
