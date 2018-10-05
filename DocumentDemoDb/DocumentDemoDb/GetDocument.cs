bb14b2dcdb594e3ef6b55940b6ae26d8be5e361a
Dileep Dayanand Pai <DP00557033@techmahindra.com>
2018-10-05 12:18:39 +0530

    Entered Document Db

---
+++ DocumentDemoDb/DocumentDemoDb/GetDocument.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DocumentDemoDb
+{
+    class GetDocument
+    {
+       
+        public  static Document GetDocumentById(DocumentClient Client, string Id)
+        {
+            return Client.CreateDocumentQuery(GlobalIdentifiers.MyLinkCollection)
+                   .Where(e => e.Id == Id)
+                   .AsEnumerable()
+                   .First();
+        }
+    }
+}
---
+++ DocumentDemoDb/DocumentDemoDb/GlobalIdentifiers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+
+namespace DocumentDemoDb
+{
+    public static class GlobalIdentifiers
+    {
+
+
+        public static string EndPoint = "<Document Db End Point obtained from Portal>";
+        //Environment.GetEnvironmentVariable("EndPointOfDb");
+        public static string AuthKey = "<Document Db connection String from Portal>";
+            //Environment.GetEnvironmentVariable("AuthKey");
+        public static string MyLinkCollection = "dbs/your Db name/colls/your coloumn name/";
+            //Environment.GetEnvironmentVariable("MyLinkCollection");
+
+    }
+}
